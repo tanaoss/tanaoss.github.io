ae195c12cd1fb39406322144e764df19756d5199
谭澳 <1786325243@qq.com>
2019-04-26 23:40:29 +0800

    Site updated: 2019-04-26 23:40:26

--- js/src/post-details.js
+++ js/src/post-details.js
@@ -30,7 +30,14 @@ $(document).ready(function () {
   }
 
 });
-
+$(document).ready(function(){
+    $(document).on('click', '.fold_hider', function(){
+        $('>.fold', this.parentNode).slideToggle();
+        $('>:first', this).toggleClass('open');
+    });
+    //默认情况下折叠
+    $("div.fold").css("display","none");
+});
 $(document).ready(function () {
   var html = $('html');
   var TAB_ANIMATE_DURATION = 200;
